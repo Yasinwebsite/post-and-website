4990fdddc66c7099ec3f1fec376f104f5b421907
YasinMirhabibi <mirhabibiyasin63@gmail.com>
2026-01-07 09:34:41 +0330

    Update 3

--- BlogPost/models.py
+++ BlogPost/models.py
@@ -4,8 +4,11 @@ from django.db import models
 # Create your models here.
 
 class blogPost(models.Model):
+    
     title = models.CharField(max_length=100)
     desc = models.TextField()
     likes = models.IntegerField(max_length=2)
     Publish = models.DateTimeField(auto_now_add=True)
     Update = models.DateTimeField(auto_now=True)
+    
+    
